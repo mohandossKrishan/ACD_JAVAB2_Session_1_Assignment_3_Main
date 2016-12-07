3f9e1a8cab9cf6880de3490b2a154e8020b6c148
mohandossKrishan <mohandoss28@gmail.com>
2016-12-07 16:26:42 +0530

    SwappingTwoNumbers

--- SwappingTwoNumbers.java
+++ SwappingTwoNumbers.java
@@ -1,13 +1,13 @@
-class SwappingTwoNumbers
+public class SwappingTwoNumbers
 {  
 	public static void main(String args[])
 	{  
 		int a = 25;
                 int b = 39;
-		
+		System.out.println("Before swapping: a = "+ a + " b = " + b );
 		a = a + b;
 		b = a-b;
 		a = a-b;
-		System.out.println("After swapping: a = "+ a + " b = " + b );
+		System.out.println("After  swapping: a = "+ a + " b = " + b );
    	}  
 }
